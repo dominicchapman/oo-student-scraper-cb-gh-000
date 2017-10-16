a8ff8a5d5baa6a1e750530a992ab1a019e1985d0
Dominic Chapman <dominicchapman@me.com>
2017-10-16 18:33:09 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,14 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     binding.pry
+    # :profile_url
+    # doc.css('.student-card a').first.attr('href')
+
+    # :name
+    # doc.css('.student-name').first.text
+
+    # :location
+    # doc.css('.student-location').first.text
   end
 
   def self.scrape_profile_page(profile_url)
