5960a175752e756d940e332bd9cbcf061e030a20
Dominic Chapman <dominicchapman@me.com>
2017-10-16 18:51:48 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,12 +7,16 @@ class Scraper
     doc = Nokogiri::HTML(open(index_url))
     # :profile_url
     # doc.css('.student-card a').first.attr('href')
+    # doc.css('.student-card').css('a').attr('href').value
 
     # :name
     # doc.css('.student-name').first.text
+    # doc.css('.student-card').css('.student-name').text
 
     # :location
     # doc.css('.student-location').first.text
+    # doc.css('.student-card').css('.student-location').text
+
     students = []
     binding.pry
   end
