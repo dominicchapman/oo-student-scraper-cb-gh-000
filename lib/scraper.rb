d98ac0890332f4279b27c897979539b02ad9a21b
Dominic Chapman <dominicchapman@me.com>
2017-10-16 18:18:15 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,13 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(index_url)
+    doc = Nokogiri::HTML(open(index_url))
     binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
