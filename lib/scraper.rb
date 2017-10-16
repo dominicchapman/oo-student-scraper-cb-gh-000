7f910dc3140e09cd551a3d2e04c17fe268da790f
Dominic Chapman <dominicchapman@me.com>
2017-10-16 19:52:32 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,9 +28,6 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-    # social links
-    # profile_page.css('.social-icon-container').children.css('a')
-
 
     student = {}
     doc.css('.social-icon-container a').each do |link|
